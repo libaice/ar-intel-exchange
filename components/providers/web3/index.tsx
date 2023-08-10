1301005a4b15ce63e0730c4ee572bcadde579c60
libaice <libaice147@gmail.com>
2023-08-10 18:24:45 +0800

    add basic css styles and Web3 Provider

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,7 @@
+import { createContext, FunctionComponent, useContext, useEffect, useState } from "react"
+
+
+const Web3Provider: FunctionComponent = ({ children }) => {
+}
+
+export default Web3Provider;
