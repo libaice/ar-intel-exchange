d8c1adf222d3ca3be1423517ead31aac8836b150
libaice <libaice147@gmail.com>
2023-08-15 11:50:44 +0800

    add nft List and NFT item

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -1,9 +1,19 @@
+import Navbar from "../navbar";
 import { FunctionComponent, ReactNode } from "react";
 
 const BaseLayout: FunctionComponent<{ children: ReactNode }> = ({
   children,
 }) => {
-  return <div className="py-16 bg-gray-50 overflow-hidden min-h-screen ">{children}</div>;
+  return (
+    <>
+    <Navbar/>
+      <div className="py-16 bg-gray-50 overflow-hidden min-h-screen ">
+        <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+          {children}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BaseLayout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Navbar } from "../components";
 
-import { ToastContainer } from "react-toastify";
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
   );
