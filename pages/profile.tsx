fc585aa79d9e211454d1bb47a240d66b1f67a8be
libaice <libaice147@gmail.com>
2023-08-16 13:36:18 +0800

    active link

---
+++ pages/profile.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../components";
+
+const profile: NextPage = () => {
+  return <BaseLayout>Proile</BaseLayout>;
+};
+
+export default profile;
