2c2b2c417814d8a81400c51d263a10cce6c046e1
libaice <libaice147@gmail.com>
2023-08-16 12:06:13 +0800

    update NFT Type , Nft Iten and list

--- components/nft/list/index.tsx
+++ components/nft/list/index.tsx
@@ -1,15 +1,24 @@
 import { FunctionComponent } from "react";
 import NftItem from "../item";
+import { NftMeta } from "../../../types/nft";
 
+type NftListProps = {
+  nfts: NftMeta[];
+};
 
-const NftList: FunctionComponent = () =>{
-    return(
-        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-            <NftItem/>
+const NftList: FunctionComponent<NftListProps> = ({ nfts }) => {
+  return (
+    <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      {nfts.map((nft) => (
+        <div
+          key={nft.image}
+          className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+        >
+          <NftItem item={nft} />
         </div>
-      </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default NftList;
